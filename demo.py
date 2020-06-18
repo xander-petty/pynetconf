cf50fc12662b6cfc9145088f16a8a7df41f2bbbb
xander.petty <xander.petty@protonmail.com>
2020-06-18 07:54:58 -0500

    Logically built acl

--- demo.py
+++ demo.py
@@ -15,10 +15,14 @@ from lxml.builder import E as Element
 
 if __name__ == '__main__':
     # Connect to Netconf on lab device
-    username = 'admin'
-    password = getpass('Local admin password: ')
-    ip = '10.179.0.37'
-    port = 830
+    # username = 'admin'
+    # password = getpass('Local admin password: ')
+    # ip = '10.179.0.37'
+    # port = 830
+    username = 'developer'
+    password = 'C1sco12345'
+    port = 10000
+    ip = 'ios-xe-mgmt.cisco.com'
     netconf = manager.connect(host=ip, username=username, password=password, port=port, hostkey_verify=False)
 
     # Demonstrate pulling ARP data from Operational state
@@ -110,23 +114,90 @@ if __name__ == '__main__':
             )
         ), pretty_print=True
     ).decode()
-    interface_xml = netconf.get_config(source='running', filter=interface_filter).xml
+    interface_xml = netconf.get_config(choice='running', filter=interface_filter).xml
     interface_dict = xmltodict.parse(interface_xml)['rpc-reply']['data']['interfaces']['interface']
     interface_json = json.loads(json.dumps(interface_dict))
     pprint(interface_json)
 
-    # Demonstrate pulling line config information from the running config 
+    # Demonstration of pulling line card information from Operational state
     line_filter = etree.tostring(
         Element(
             'filter',
             Element(
-                'native',
+                'linecard-oper-data',
                 Element(
-                    'line',
+                    'linecard'
+                ),
+                xmlns='http://cisco.com/ns/yang/Cisco-IOS-XE-linecard-oper'
+            )
+        ), pretty_print=True
+    ).decode()
+    line_xml = netconf.get(filter=line_filter).xml
+    line_dict = xmltodict.parse(line_xml)['rpc-reply']['data']['linecard-oper-data']['linecard']
+    line_json = json.loads(json.dumps(line_dict))
+    pprint(line_json)
+
+    # Testing of building config differently
+    def src_dst_choice(*args):
+        choice = []
+        for e in args:
+            if str(type(e)) == "<class 'dict'>":
+                choice.append(
                     Element(
-                        'line-list'
+                        list(e.items())[0][0],
+                        list(e.items())[0][1]
                     )
                 )
-            )
-        )
-    )
+            elif str(type(e)) == "<class 'str'>":
+                choice.append(
+                    Element(
+                        e
+                    )
+                )
+            elif str(type(e)) == "<class 'list'>":
+                for i in e:
+                    if str(type(i)) == "<class 'dict'>":
+                        choice.append(
+                            Element(
+                                list(i.items())[0][0],
+                                list(i.items())[0][1]
+                            )
+                        )
+                    elif str(type(i)) == "<class 'str'>":
+                        choice.append(
+                            Element(
+                                i
+                            )
+                        )
+        return choice 
+
+    xml_seq_rules = Element('access-list-seq-rule')
+    rule10 = (Element('sequence', '00000010'), Element('ace-rule'))
+    temp = src_dst_choice('permit', 'ip', {'source-address': '10.0.0.0'}, {'source-wildcard-bits': '0.0.0.255'}, 'destination-any')
+    for rule in temp:
+        rule10[1].append(rule)
+    xml_seq_rules.append(rule10[0])
+    xml_seq_rules.append(rule10[1])
+
+    rule20 = (Element('sequence', '00000020'), Element('ace-rule'))
+    temp = src_dst_choice('permit', 'ip', {'source-address': '20.0.0.0'}, {'source-wildcard-bits': '0.0.0.255'}, {'destination-address': '0.0.0.0'}, {'destination-wildcard-bits': '255.255.255.255'})
+    for rule in temp:
+        rule20[1].append(rule)
+    xml_seq_rules.append(rule20[0])
+    xml_seq_rules.append(rule20[1])
+
+    rule30 = (Element('sequence', '00000030'), Element('ace-rule'))
+    temp = src_dst_choice('deny', 'ip', 'source-any', 'destination-any', 'log')
+    for rule in temp:
+        rule30[1].append(rule)
+    xml_seq_rules.append(rule30[0])
+    xml_seq_rules.append(rule30[1])
+
+    acl_name = Element('test_acl')
+    acl_extended = Element('extended', acl_name, xml_seq_rules, xmlns='http://www.cisco.com/ns/yang/Cisco-IOS-XE-acl')
+
+    xml_ip = Element('ip', acl_extended)
+    xml_native = Element('native', xml_ip, xmlns='http://www.cisco.com/ns/yang/Cisco-IOS-XE-native')
+    xml_config = Element('config', xml_native)
+    xml_string = etree.tostring(xml_config, pretty_print=True).decode()
+    print(xml_string)
